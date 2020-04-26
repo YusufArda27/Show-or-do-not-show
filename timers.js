0c7f7c6fa114975f6ac421b082e08d3a863cfc0b
YusufArda27 <58040592+YusufArda27@users.noreply.github.com>
2020-04-26 15:42:53 +0300

    Add files via upload

---
+++ timers.js
@@ -0,0 +1,12 @@
+﻿var a=document.getElementById("btn");
+var b=document.getElementById('content')
+a.onclick=function(){
+  if(b.className=="show"){
+    b.className="";
+    a.innerText="Show"
+  }else{
+    b.className="show";
+    a.innerText="Don not Show"
+  }
+
+};
